dde662de23e5dd58983544cf08332e91b0ace7ea
Izaac Molina <izaac.6.30@gmail.com>
2023-07-06 17:07:46 +0000

    Added functionality for web hook

--- acnh_frontend.py
+++ acnh_frontend.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, SubmitField
 from wtforms.validators import DataRequired, NumberRange
 from flask_behind_proxy import FlaskBehindProxy
+import git
 
 
 app = Flask(__name__)
@@ -21,6 +22,16 @@ def gome():
     if form.validate_on_submit(): # checks if entries are valid
         flash(f'Data successfully retrieved for {form.villagerID.data}!')
     return render_template('home.html', title='Home Search', form=form)
+
+@app.route("/update_server", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/CHANGE_TO_PYTHON_ANYWHERE_USERNAME/CHANGE_TO_GITHUB_REPO_NAME')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
     
 
 if __name__ == '__main__':
